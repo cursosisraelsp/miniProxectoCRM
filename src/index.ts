8026defe8605b4bd1f2776d1a2c44732bca6a149
Fran <powebicreacion@gmail.com>
2025-03-24 20:03:54 +0100

    Index ts

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,37 @@ import { Comunicacion } from "./clases/Comunicacion";
 //import fondo from "./fondoBueno.png";
 import './css/main.css'; // Temos que integrar o CSS para que webpack o compile
 
-if(location.pathname == "/paxina-app"){
-    const refBotonGET: HTMLButtonElement = document.querySelector("#solicitudeGET");
-    const refBotonPOST: HTMLButtonElement = document.querySelector("#solicitudePOST")
-    
-    // INTRODUZCO DOUS BOTÓNS SIMULANDO 'EVENTOS' 
-    // UN POST
-    // UN GET 
-    
+if (location.pathname === "/paxina-app") {
+    const refBotonGET: HTMLButtonElement | null = document.querySelector("#solicitudeGET");
+    const refBotonPOST: HTMLButtonElement | null = document.querySelector("#solicitudePOST");
 
-    refBotonGET.addEventListener("click",async () =>{
-        let endpoint = "/recibo-datos-do-servidor";
-        await Comunicacion.metodoGet(endpoint)
-        console.log(Comunicacion.respostaServidor)
-        // UTILIZARÍAMOS O DATO QUE CHEGA DO SERVIDOR PARA PINTAR
-    })
-    refBotonPOST.addEventListener("click",async ()=>{
-        let endpoint = "/envio-datos-o-servidor"
-        await Comunicacion.metodoPost(endpoint)
-        console.log(Comunicacion.respostaServidor)
-        // UTILIZARÍAMOS O DATO QUE CHEGA DO SERVIDOR PARA PINTAR
-    })
+    if (refBotonGET && refBotonPOST) {
+        // INTRODUZCO DOUS BOTÓNS SIMULANDO 'EVENTOS' 
+        // UN POST
+        // UN GET 
+
+        refBotonGET.addEventListener("click", async () => {
+            try {
+                let endpoint = "/recibo-datos-do-servidor";
+                await Comunicacion.metodoGet(endpoint);
+                console.log(Comunicacion.respostaServidor);
+                // UTILIZARÍAMOS O DATO QUE CHEGA DO SERVIDOR PARA PINTAR
+            } catch (error) {
+                console.error("Error en la solicitud GET:", error);
+            }
+        });
+
+        refBotonPOST.addEventListener("click", async () => {
+            try {
+                let endpoint = "/envio-datos-o-servidor";
+                await Comunicacion.metodoPost(endpoint);
+                console.log(Comunicacion.respostaServidor);
+                // UTILIZARÍAMOS O DATO QUE CHEGA DO SERVIDOR PARA PINTAR
+            } catch (error) {
+                console.error("Error en la solicitud POST:", error);
+            }
+        });
+    } else {
+        console.error("Los botones no fueron encontrados en el DOM");
+    }
 }
