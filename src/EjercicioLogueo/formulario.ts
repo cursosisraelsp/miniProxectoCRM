43885194ef41749c775344f77a1c58c060d543a9
Fran <powebicreacion@gmail.com>
2025-03-24 18:31:17 +0100

    Formulario.ts

--- src/EjercicioLogueo/formulario.ts
+++ src/EjercicioLogueo/formulario.ts
@@ -1,32 +1,22 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-private metodoRefFormulario (){
+*/
+private refFormulario: string;
+private datosFormulario: FormData;
+private datosTranformadosObxeto:any;
+constructor(referencia:string){
 
 }
+public metodoAccionFormulario(){
+this.metodoRefformulario()
+this.metodoDatosFormulario()
+this.metodoPreparandoObxetoEnvio()
+}
+private metodoRefFormulario(){
+
+}
+private metodoDatosFormulario(){
+
+}
+
 private metodoDatosObxetoEnvio (){
     let datosFormArrayArrays = this.datosFormulario.entries();
     this.datosFormTranformadosObxecto = obxect.formEntries (datosFormArrayArrays);
