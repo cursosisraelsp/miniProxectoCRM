02a7c0d4cdbce70c9fd589efd29eb05c5720029d
CasafutbolGit <powebicreacion@gmail.com>
2025-03-25 08:54:31 +0100

    Actualizando el Servidor server.js
    Añado basicamente app.use(express.json();//para interpretacion de los datos y acceder al archivo estatico

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
 const express = require("express");
 const path = require("path");
-const { paxinaApp, paxinaNoUser,paxinaLogueo, paxinaInvoices, paxinaCesta, paxinaCustomers, paxinaGraficas } = require("./controladores/views");
+const { paxinaApp, paxinaNoUser, paxinaLogueo, paxinaInvoices, paxinaCesta, paxinaCustomers, paxinaGraficas } = require("./controladores/views");
 
 const app = express();
 
 // Paxinas
-app.use(express.urlencoded({extended: true}))
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());//para interpretación de los datos
 // Accedo o arquivo estático
 app.use(express.static(path.join(__dirname, "dist")));
-//### GETTERS
-app.get("/recibo-datos-do-servidor",(req,res)=>{
-    res.send({
-        mensaxe:{
-            dato1:"Juanito",
-            dato2:"pepito",
-            dato3:3
-        }
-    })
-})
-app.get("/logueo",(req,res)=>{
-    paxinaLogueo(req,res)
-})
+
 //### POST
-app.post("/paxina-app",(req,res)=>{
-    
-    console.log("recibo dato no server",req.body)
 
-    let condicion = req.body.nome2 === 'Israel' && req.body.apelido2 === 'mariano';
-    
-    condicion ? paxinaApp(req,res) : paxinaNoUser(req,res)
 
+app.post("/logueo", (req, res) => {
+    console.log("Intento de login:", req.body);
+    const { nome, apelido } = req.body;
+
+    if(nome == "" || apelido == "" || nome == undefined || apelido == undefined){
+        res.json({ resposta: "Todos los campos deben ser rellenados" });
+    }
+
+    if (nome === "Israel" && apelido === "mariano") {
+        return res.status(200).json({ resposta: "El envío de datos fue correcto" });
+    }
+    return res.json({ resposta: "El usuario no es correcto" });
+
+
+});
+
+// GETTERS PáXINAS
+
+app.get("/home",(req,res)=>{
+    paxinaHome(req,res)
+})
+app.get("/logueo", (req, res) => {
+    paxinaLogueo(req, res)
 })
 // GETTERS PáXINAS
 app.get("/invoices",(req,res)=>{
@@ -46,12 +50,22 @@ app.get("/clientes",(req,res)=>{
 app.get("/graficas",(req,res)=>{
     paxinaGraficas(req,res)
 })
+app.get("/productos",(req,res)=>{
+    paxinaNewProduct(req,res)
+})
+app.get("/axustes",(req,res)=>{
+    paxinaAxustes(req,res)
+})
+
+
 //Un evento dende o cliente
-app.post("/envio-datos-o-servidor",(req,res)=>{
-    res.send({mensaxe:"datos enviados"})
+app.post("/envio-datos-o-servidor", (req, res) => {
+    //imprimir datos enviados por consola
+    console.log(req.body, req.body.datosEnvio);
+    res.send({ mensaxe: "datos enviados" })
 })
 //##########
 //START SERVER
 app.listen(3000, function () {
- console.log("Server running");
+    console.log("Server running");
 });
